6cd6a023c0c1ae3dd1313d82af965902b2e80337
aaronlam <aaronlam1618@gmail.com>
2018-10-13 12:15:26 -0700

    add MergeSortedArray

---
+++ MergeSortedArray.java
@@ -0,0 +1,26 @@
+public class MergeSortedArray {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        if (nums1 == null || nums2 == null) {
+            return;
+        }
+        int p1 = m - 1;
+        int p2 = n - 1;
+        int i = m + n - 1;
+        while (p1 >= 0 && p2 >= 0) {
+            if (nums1[p1] > nums2[p2]) {
+                nums1[i] = nums1[p1];
+                i = i - 1;
+                p1 = p1 - 1;
+            } else {
+                nums1[i] = nums2[p2];
+                i = i - 1;
+                p2 = p2 - 1;
+            }
+        }
+        while (p2 >= 0) {
+            nums1[i] = nums2[p2];
+            i = i - 1;
+            p2 = p2 - 1;
+        }
+    }
+}
